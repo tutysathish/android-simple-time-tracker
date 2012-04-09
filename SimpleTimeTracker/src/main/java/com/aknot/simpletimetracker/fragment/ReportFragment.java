6f4766fa630188a1f80421b629361833d66decf6
aknot13 <aknot13@gmail.com>
2012-04-09 10:54:11 +0000

    add 3 fragments for to replace the tabs activities.

---
+++ SimpleTimeTracker/src/main/java/com/aknot/simpletimetracker/fragment/ReportFragment.java
@@ -0,0 +1,23 @@
+package com.aknot.simpletimetracker.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aknot.simpletimetracker.R;
+
+public class ReportFragment extends Fragment {
+
+	@Override
+	public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		return inflater.inflate(R.layout.tab_report, container, false);
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+	}
+}
